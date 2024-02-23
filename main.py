df5386d7aada53a676e3164d9581c350f3764fbd
Nennneko5787 <nennneko5787@gmail.com>
2024-02-23 12:35:30 +0900

    うわあああああああああ

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import discord
 from discord import app_commands
 import aiohttp
 from keep_alive import keep_alive
+from discord.ext import tasks
+import re
+import asyncio
 
 if os.path.isfile(".env") == True:
 	from dotenv import load_dotenv
@@ -19,8 +22,8 @@ tree = app_commands.CommandTree(client)
 @client.event
 async def on_ready():
 	print("Ready!")
+	server_stat.start()
 	await tree.sync()	#スラッシュコマンドを同期
-
 
 @tree.command(name="ping", description="ping")
 async def ping(interaction: discord.Interaction):
@@ -45,6 +48,39 @@ async def mcstart(interaction: discord.Interaction):
 				await interaction.followup.send("起動をリクエストしました。起動まで時間がかかるので、しばらくお待ち下さい...")
 			else:
 				await interaction.followup.send(f"起動のリクエストに失敗しました。( エラーコード **{response.status}** )")
+
+@tasks.loop(minutes=20)
+async def server_stat():
+	guild = client.get_guild(1208388325954560071)
+	auth = len(guild.get_role(1208388351372169256).members)
+
+	# 参加人数
+	channel = client.get_channel(1210425024356286464)
+	await channel.edit(name=re.sub(r'(\D*)\d+', f'\g<1>{len(guild.members)}', channel.name))
+
+	await asyncio.sleep(2)
+
+	# メンバー
+	channel = client.get_channel(1210425116521930802)
+	await channel.edit(name=re.sub(r'(\D*)\d+', f'\g<1>{len([member for member in guild.members if not member.bot])}', channel.name))
+
+	await asyncio.sleep(2)
+
+	# 認証済みメンバー
+	channel = client.get_channel(1210425271057121331)
+	await channel.edit(name=re.sub(r'(\D*)\d+', f'\g<1>{auth}', channel.name))
+
+	await asyncio.sleep(2)
+
+	# 非認証メンバー
+	channel = client.get_channel(1210425314950520912)
+	await channel.edit(name=re.sub(r'(\D*)\d+', f'\g<1>{len([member for member in guild.members if not member.bot]) - auth}', channel.name))
+
+	await asyncio.sleep(2)
+
+	# ボット
+	channel = client.get_channel(1210425157168926731)
+	await channel.edit(name=re.sub(r'(\D*)\d+', f'\g<1>{len([member for member in guild.members if member.bot])}', channel.name))
 
 keep_alive()
 client.run(token)
