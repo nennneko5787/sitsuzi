95ec46da326abbfaafda2230b79c2ee15a5b3ab7
nennneko5787 <nennneko5787@gmail.com>
2024-03-05 00:05:00 +0900

    うおおおおお

--- main.py
+++ main.py
@@ -115,23 +115,6 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	if message.author.bot == False:
-		try:
-			# テーブルからexpの値を取得
-			loop = asyncio.get_event_loop()
-			data, count = await loop.run_in_executor(None,supabase.table('member_data').select("*").eq("id",message.author.id).execute)
-			exp = data[0][0]["exp"]
-			level = data[0][0]["level"]
-	
-			exp = exp + random.uniform(0, 5)
-			if exp >= (35 * level):
-				level = level + 1
-				await client.get_channel(1208722087032651816).send(f"🥳 **{message.author.mention}** さんのレベルが **{level - 1}** から **{level}** に上がりました 🎉")
-	
-			data, count = await loop.run_in_executor(None,supabase.table('member_data').upsert({'id': message.author.id, 'exp': exp, 'level': level}).execute)
-	except:
-		pass
-
 	if message.channel.id == 1210867877641457704:
 		if message.author.bot == False:
 			global_prom_1 = "あなたは、身長160cm、体重135kgの、とある喫茶店で私の専属メイドとして働いている女の子です。"\
@@ -232,6 +215,22 @@ async def on_message(message):
 							file = discord.File(image_stream, filename=f"bokete{extension}")
 							await message.reply(f"# {title}\n{odai}\nこのボケは {date} に投稿されました\nID: {random_int}", file=file)
 
+	if message.author.bot == False:
+		try:
+			# テーブルからexpの値を取得
+			loop = asyncio.get_event_loop()
+			data, count = await loop.run_in_executor(None,supabase.table('member_data').select("*").eq("id",message.author.id).execute)
+			exp = data[0][0]["exp"]
+			level = data[0][0]["level"]
+	
+			exp = exp + random.uniform(0, 5)
+			if exp >= (35 * level):
+				level = level + 1
+				await client.get_channel(1208722087032651816).send(f"🥳 **{message.author.mention}** さんのレベルが **{level - 1}** から **{level}** に上がりました 🎉")
+	
+			data, count = await loop.run_in_executor(None,supabase.table('member_data').upsert({'id': message.author.id, 'exp': exp, 'level': level}).execute)
+	except:
+		pass
 
 @tree.command(name="deletemsghistory", description="AIとの会話の履歴を削除します")
 async def deletemsghistory(interaction: discord.Interaction, user: discord.Member = None):
