b2b8116873edc437b324258b493567374d7e29d6
nennneko5787 <nennneko5787@gmail.com>
2024-03-13 20:35:00 +0900

    すみません...

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ async def on_ready():
 	hour.start()
 	spla3.start()
 	change.start()
-	await client.get_guild(1208388325954560071).get_channel(1208388344044593192).send(f"{client.user.mention} が、`{os.getenv("RENDER_GIT_COMMIT")}`へのアップデート作業に入ります。そのまま5分ほどお待ち下さい。(この間にレベルアップやログインボーナスの受け取り、ガチャを回すなどの動作を行うと二重に反応してしまいます。仕様です。バグ報告しないでください。)")
+	await client.get_guild(1208388325954560071).get_channel(1208388344044593192).send(f"{client.user.mention} が、`{os.getenv('RENDER_GIT_COMMIT')}`へのアップデート作業に入ります。そのまま5分ほどお待ち下さい。(この間にレベルアップやログインボーナスの受け取り、ガチャを回すなどの動作を行うと二重に反応してしまいます。仕様です。バグ報告しないでください。)")
 
 @client.event
 async def on_member_update(before, after):
