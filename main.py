c96c95a81625e6e7d02b76d284957e1f5cdea6f7
nennneko5787 <nennneko5787@gmail.com>
2024-03-15 15:51:00 +0900

    global

--- main.py
+++ main.py
@@ -214,6 +214,7 @@ async def on_ready():
 	hour.start()
 	spla3.start()
 	change.start()
+	global is_connected
 	if is_connected == False:
 		await client.get_guild(1208388325954560071).get_channel(1218087342397591553).send(f"{client.user.mention} が、`{os.getenv('RENDER_GIT_COMMIT')}`へのアップデート作業に入ります。そのまま5分ほどお待ち下さい。(この間にレベルアップやログインボーナスの受け取り、ガチャを回すなどの動作を行うと二重に反応してしまいます。仕様です。バグ報告しないでください。)")
 		is_connected = True
