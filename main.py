879759debb58942fdd47ecd396f4420e37b53eed
nennneko5787 <nennneko5787@gmail.com>
2024-03-19 11:52:54 +0900

    君だけがすべてさ

--- main.py
+++ main.py
@@ -832,6 +832,7 @@ async def setbirthday(interaction: discord.Interaction, person: str, date: str):
 		)
 		await interaction.followup.send("誕生日をセットしました。")
 	except:
+		error = traceback.format_exc()
 		await interaction.followup.send("誕生日の書き方がおかしいらしい。")
 
 @tree.command(name="mcstart", description="Minecraftサーバーを起動します")
