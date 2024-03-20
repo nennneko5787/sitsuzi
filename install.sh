98df0e1f4189d72cbb24a664fe3abb69d70bbd48
nennneko5787 <nennneko5787@gmail.com>
2024-03-20 17:29:34 +0900

    debian草

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cat /etc/os-release
-sudo apt update
-sudo apt upgrade
-sudo apt install language-pack-ja
+apt update
+apt upgrade
+apt -y install task-japanese locales-all
 pip install -r requirements.txt
